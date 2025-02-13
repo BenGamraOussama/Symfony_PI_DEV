00cce8014433a196ec2373e619b8ee6748acb8ee
Fares belgacem <fares.belgacem@esprit.tn>
2025-02-13 01:54:43 +0100

    crud fares

---
+++ src/Entity/Commande.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CommandeRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CommandeRepository::class)]
+class Commande
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $date_commande = null;
+
+    #[ORM\Column]
+    private ?float $montant_total = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDateCommande(): ?\DateTimeInterface
+    {
+        return $this->date_commande;
+    }
+
+    public function setDateCommande(\DateTimeInterface $date_commande): static
+    {
+        $this->date_commande = $date_commande;
+
+        return $this;
+    }
+
+    public function getMontantTotal(): ?float
+    {
+        return $this->montant_total;
+    }
+
+    public function setMontantTotal(float $montant_total): static
+    {
+        $this->montant_total = $montant_total;
+
+        return $this;
+    }
+}
