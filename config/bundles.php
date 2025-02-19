2701275578fe0b2d299ef249b501e7c47df6eb25
Fares belgacem <fares.belgacem@esprit.tn>
2025-02-19 02:34:29 +0100

    Ajout de tous les fichiers et dernières modifications

--- config/bundles.php
+++ config/bundles.php
@@ -3,6 +3,9 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> c5a8582 (Add webapp packages)
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
@@ -14,7 +17,10 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+<<<<<<< HEAD
     SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
 =======
 >>>>>>> 5f40180 (Add initial set of files)
+=======
+>>>>>>> c5a8582 (Add webapp packages)
 ];
