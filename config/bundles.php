4dd8d5ae480e0c927cfdda89f27095bd363b5c99
Fares belgacem <fares.belgacem@esprit.tn>
2025-02-19 01:37:53 +0100

    Mise à jour du fichier edit.html.twig et autres modifications

--- config/bundles.php
+++ config/bundles.php
@@ -2,6 +2,7 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+<<<<<<< HEAD
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
@@ -14,4 +15,6 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
+=======
+>>>>>>> 5f40180 (Add initial set of files)
 ];
